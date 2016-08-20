a552cc8907f45efcc3f5e1351479089abd71a835
Ant Horner <antdh2@gmail.com>
2016-08-20 12:16:29 +0100

    Housekeeping

--- autotask/settings.py
+++ autotask/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'autotask_api_app',
     'autotask_web_app',
     'account',
     'djstripe',
