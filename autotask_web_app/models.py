1781ef3de3c17651d48f3d8003034ba7de5e4a60
Ant Horner <antdh2@gmail.com>
2016-08-23 01:04:06 +0100

    Model/DB changes for Django admin

--- autotask_web_app/models.py
+++ autotask_web_app/models.py
@@ -29,16 +29,22 @@ class Profile(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.first_name + " " +self.last_name
+        return self.first_name + " " + self.last_name
 
 class Picklist(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     key = models.CharField(max_length=254)
     value = models.CharField(max_length=254)
 
+    def __str__(self):
+        return str(self.id) + ": " + self.company.name
+
 class Entity(models.Model):
     name = models.CharField(max_length=254)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name + ": " + self.profile.first_name + " " + self.profile.last_name
 
 class ValidationGroup(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
@@ -46,7 +52,7 @@ class ValidationGroup(models.Model):
     entity = models.ForeignKey(Entity, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.company
+        return self.company.name + ": " + self.name
 
 class ValidationGroupRule(models.Model):
     entity = models.ForeignKey(Entity, on_delete=models.CASCADE)
@@ -58,7 +64,7 @@ class ValidationGroupRule(models.Model):
     company = models.ForeignKey(Company)
 
     def __str__(self):
-        return self.company + ": " + self.key + " " + self.operator + " " + self.value
+        return self.company.name + ": " + self.key + " " + self.operator + " " + self.value
 
 class Validation(models.Model):
     entity = models.ForeignKey(Entity, on_delete=models.CASCADE)
@@ -70,4 +76,4 @@ class Validation(models.Model):
     mandatory = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.id + ": " + self.validation_group
+        return "Validation #" + str(self.id) + " for group " + self.validation_group.name
