51b560c471e7278fd167a391db1505fc816a3bb5
Ant Horner <antdh2@gmail.com>
2016-08-25 15:46:53 +0100

    Fix user signup bug

--- autotask_web_app/models.py
+++ autotask_web_app/models.py
@@ -28,8 +28,7 @@ class Profile(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.first_name + " " + self.last_name
+
 
 class Picklist(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
